09bce19eae664f9e6d0bb5398ad2ccf7f5274bbb
Andreas Schreiber <andreas.schreiber@dlr.de>
2020-07-13 19:03:38 +0200

    Beautifies the code.

--- bin/proxy.js
+++ bin/proxy.js
@@ -1,14 +1,14 @@
 /* The proxy server, to run as root on port 80 */
 var http = require('http'),
-    httpProxy = require('http-proxy');
+  httpProxy = require('http-proxy');
 /* Default: 80 */
 var proxyPort = 80;
 /* Default: 8905 */
-var port = 8905; 
-if(process.argv[2]){
+var port = 8905;
+if (process.argv[2]) {
   proxyPort = parseInt(process.argv[2]);
 }
-if(process.argv[3]){
+if (process.argv[3]) {
   port = parseInt(process.argv[3]);
 }
 console.log("Redirecting port " + proxyPort + " to port " + port + ".");
